3887714fd564598615e2cbede02773c693a09ea8
akkireddytarun06 <tarunakkireddy@mirafra.com>
2025-04-16 13:31:46 +0530

    day1 problem3 functional pointers

--- main.c
+++ main.c
@@ -1,78 +1,42 @@
+// Online C compiler to run C program online
 #include <stdio.h>
-
-
-typedef int (*Comparator)(int, int);
-
-
-void swap(int *a, int *b) {
-    int temp=*a;
-    *a=*b;
-    *b=temp;
-}
-
-int partition(int arr[], int low, int high, Comparator cmp) {
-    int pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-        if (cmp(arr[j], pivot) < 0) {
-            i++;
-            swap(&arr[i], &arr[j]);
+typedef int (*opratorptr)(int ,int);
+char operators[]={'+','-','*','/'};
+int operatorcheck(char c)
+{
+    for(int i=0;i<sizeof(operators);i++)
+    {
+        if(c==operators[i])
+        {
+            return i;
         }
     }
-
-    swap(&arr[i + 1], &arr[high]);
-    return i + 1;
 }
-
-void quickSort(int arr[], int low, int high, Comparator cmp) {
-    if (low < high) {
-        int pi = partition(arr, low, high, cmp);
-
-        quickSort(arr, low, pi - 1, cmp);
-        quickSort(arr, pi + 1, high, cmp);
-    }
+int add(int a,int b)
+{
+    return a+b;
 }
-
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+int sub(int a,int b)
+{
+    return a-b;
 }
-
-int ascending(int a, int b) {
-    return a - b;
+int mul(int a,int b)
+{
+    return a*b;
 }
-
-int descending(int a, int b) {
-    return b - a;
+int div(int a,int b)
+{
+    return a/b;
 }
-
-int evenFirst(int a, int b) {
-    if (a % 2 == 0 && b % 2 != 0) return -1;
-    if (a % 2 != 0 && b % 2 == 0) return 1;
-    return a - b; 
-}
-
+opratorptr table[]={add,sub,mul,div};
 int main() {
-    int arr[] = {9, 3, 4, 2, 8, 5, 1, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original: ");
-    printArray(arr, n);
-
-    quickSort(arr, 0, n - 1, ascending);
-    printf("Ascending: ");
-    printArray(arr, n);
-
-    quickSort(arr, 0, n - 1, descending);
-    printf("Descending: ");
-    printArray(arr, n);
-
-    quickSort(arr, 0, n - 1, evenFirst);
-    printf("Even First: ");
-    printArray(arr, n);
-
-    return 0;
+    int a,b;
+    char c;
+    printf("enter in <int> <op> <int> format\n");
+   while(1)
+   {
+       scanf("%d%c%d",&a,&c,&b);
+       int val=operatorcheck(c);
+       printf("result is : %d\n",table[val](a,b));
+   }
 }
-
