c2c13113bb0dd9d7b9d4f6c5a0594a8d97acc65b
puttabhavitha22 <bhavithaputta@mirafra.com>
2025-04-17 17:16:12 +0530

    Update ass3.c
    Math Expression Evaluator

--- ass3.c
+++ ass3.c
@@ -3,64 +3,65 @@
 #include<ctype.h>
 int add(int a,int b)
 {
-return (a+b);
+	return (a+b);
 }
 int sub(int a,int b)
 {
-return (a-b);
+	return (a-b);
 }
 int mul(int a,int b)
 {
-return (a*b);
+	return (a*b);
 }
 int divi(int a,int b)
 {
-return (a/b);
+	return (a/b);
 }
 int main()
 {
-int (*fun[4])(int ,int)={&add,&sub,&mul,&divi};
-  char str[10],num1[10],num2[10];
-  printf("Enter the string:");
-  scanf("%[^\n]",str);
-  int i=0,j=0;
-  while(isdigit(str[i]))
-  {
-  num1[j++]=str[i++];
-  }
-  num1[j]='\0';
-  char op=str[i++];
-  j=0;
-  while(isdigit(str[i]))
-  {
-  num2[j++]=str[i++];
-  }
-  num2[j]='\0';
-  int a=atoi(num1);
-  int b=atoi(num2);
-  switch(op)
-  {
-  case '+':
-  {
-         printf("%d\n",fun[0](a,b));
-         break;
-   }
-   case '-':
-  {
-         printf("%d\n",fun[1](a,b));
-         break;
-   }
-   case '*':
-  {
-         printf("%d\n",fun[2](a,b));
-         break;
-   }
-   case '/':
-  {
-         printf("%d\n",fun[3](a,b));
-         break;
-   }
-   }
-   }
+	int (*fun[4])(int,int)= {&add,&sub,&mul,&divi};
+	char str[10],num1[10],num2[10];
+	printf("Enter the string:");
+	scanf("%[^\n]",str);
+	int i=0,j=0;
+	while(isdigit(str[i]))
+	{
+		num1[j++]=str[i++];
+	}
+	num1[j]='\0';
+	char op=str[i++];
+	j=0;
+	while(isdigit(str[i]))
+	{
+		num2[j++]=str[i++];
+	}
+	num2[j]='\0';
+	int a=atoi(num1);
+	int b=atoi(num2);
+	switch(op)
+	{
+	case '+':
+	{
+		printf("%d\n",fun[0](a,b));
+		break;
+	}
+	case '-':
+	{
+		printf("%d\n",fun[1](a,b));
+		break;
+	}
+	case '*':
+	{
+		printf("%d\n",fun[2](a,b));
+		break;
+	}
+	case '/':
+	{
+		printf("%d\n",fun[3](a,b));
+		break;
+	}
+	}
+}
+
            
   
