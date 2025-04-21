e2ae9f448ba7daeaeff7aebab68e9e11261a6013
Vishnu-del12 <chinthanaboinavishnu@mirafra.com>
2025-04-21 09:32:10 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -8,9 +8,30 @@ int sub(int a,int b)
 {
     return a-b;
 }
-int main()
+int mul(int a,int b)
 {
-    int (*fp[2])(int,int)={add,sub};
-    printf("%d",fp[0](1,2));
-    printf("%d",fp[1](1,2));
+    return a*b;
+}
+int main(){
+    int(*fp[3])(int,int)={add,sub,mul};
+    int a,b;
+    char ch;
+     printf("enter any symbol between '+' or '-' or '*'\n");
+    scanf("%c",&ch);
+    printf("enter a nd b:");
+    scanf("%d %d",&a,&b);
+   
+    switch(ch)
+    {
+        case '+':
+                    printf("%d",fp[0](a,b));
+                    break;
+        case '-':
+                    printf("%d",fp[1](a,b));
+                    break;
+        case '*':
+                    printf("%d",fp[2](a,b));
+                    break;
+        default:printf("invalid");
+    }
 }
