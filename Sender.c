2f60d26788aca62358b01e499c7cad565644b224
AravinthIyyappan <aravinthiyyappan@mirafra.com>
2025-04-23 09:35:29 +0530

    Sender.c

---
+++ Sender.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<unistd.h>
+
+int main(){
+pid_t pid;
+printf("enter pid to send SIGUSR1: ");
+scanf("%d", &pid);
+
+kill(pid, SIGUSR1);
+printf("Sent SIGUSR1 to PID %d \n", pid);
+return 0;
+}
