3002182bc1eade4559da14c785f5aa45cb6178e9
Teja6143 <tejadasari@mirafra.com>
2025-07-27 21:53:24 +0530

    Add files via upload

---
+++ june4/shared_me_wr.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+
+
+#define SHM_SIZE 1024 //shared memory size
+                      
+
+int main()
+{
+  key_t key = ftok("shmfile",65);//generate a unique key
+  int shmid = shmget(key,SHM_SIZE,0666 | IPC_CREAT);
+
+  if(shmid == -1)
+  {
+    perror("shmget failed");
+    exit(1);
+  }
+
+  char *str = (char *)shmat(shmid,NULL,0); //attach to shared memory
+  if(str == (char *)-1)
+  {
+    perror("shmat failed");
+    exit(1);
+  }
+
+  printf("Write data: ");
+  fgets(str,SHM_SIZE,stdin); //write data to shared memory
+
+  printf("Data written in memory : %s\n",str);
+
+  shmdt(str);
+
+  return 0;
+}
